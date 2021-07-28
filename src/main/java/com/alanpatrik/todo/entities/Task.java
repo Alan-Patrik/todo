8ebfda3d505024043185715d15c91023f83e8778
Alan-Patrik <alanpatrik.fragozo@gmail.com>
2021-07-28 20:38:44 -0300

    adding exceptions

--- src/main/java/com/alanpatrik/todo/entities/Task.java
+++ src/main/java/com/alanpatrik/todo/entities/Task.java
@@ -1,6 +1,5 @@
 package com.alanpatrik.todo.entities;
 
-import java.io.Serializable;
 import java.time.Instant;
 import java.util.Date;
 
---
+++ src/main/java/com/alanpatrik/todo/exception/controller/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.alanpatrik.todo.exception.controller;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.alanpatrik.todo.exception.service.ControllerNotFoundException;
+import com.alanpatrik.todo.exception.service.DatabaseException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ControllerNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ControllerNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database err";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
